06e99be91afe97da78f248794d7ebe31d5731532
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2024-03-27 11:02:58 -0300

    feat: create componet ShopCartInfo

---
+++ src/components/shopCartInfo/index.tsx
@@ -0,0 +1,7 @@
+export const ShopCartInfo = () => {
+  return (
+    <p className="font-display text-base font-light leading-6">
+      Total (3 produtos) <span className="font-medium">R$161,00</span>
+    </p>
+  )
+}
