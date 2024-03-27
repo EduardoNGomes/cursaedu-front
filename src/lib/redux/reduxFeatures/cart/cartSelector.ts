15c43d148e1a2d857a838f759cebfdfe89a046c8
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2024-03-27 13:41:14 -0300

    feat: implement redux toolkit: store, hook, provider, cartSelector,cartSlice

---
+++ src/lib/redux/reduxFeatures/cart/cartSelector.ts
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export const selectorTotalValue = (state: any) => {
+  return state.cartReducer.products.reduce(
+    (ac: any, cc: any) => ac + cc.price * cc.quantity,
+    0,
+  )
+}
+
+export const selectorTotalProductsQuantity = (state: any) => {
+  return state.cartReducer.products.reduce(
+    (ac: any, cc: any) => ac + cc.quantity,
+    0,
+  )
+}
---
+++ src/lib/redux/reduxFeatures/cart/cartSlice.ts
@@ -0,0 +1,50 @@
+import { ProductPropsWithQuantity } from '@/@types/productPropsWithQuantity'
+import { PayloadAction, createSlice } from '@reduxjs/toolkit'
+
+export interface CartState {
+  products: ProductPropsWithQuantity[]
+}
+
+const initialState: CartState = {
+  products: [],
+}
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    increaseItem: (state, action: PayloadAction<ProductPropsWithQuantity>) => {
+      const productExist = state.products.findIndex(
+        (product) => product.id === action.payload.id,
+      )
+      if (productExist > -1) {
+        const newProduct = state.products.splice(0, productExist)
+        state.products = [...newProduct, { ...action.payload }]
+      } else {
+        state.products = [...state.products, { ...action.payload }]
+      }
+    },
+
+    decreaseItem: (state, action: PayloadAction<ProductPropsWithQuantity>) => {
+      const filteredProducts = state.products.filter(
+        (product) => product.id !== String(action.payload.id),
+      )
+      state.products = filteredProducts
+    },
+    changeItemQuantity: (
+      state,
+      action: PayloadAction<ProductPropsWithQuantity>,
+    ) => {
+      state.products = state.products.map((product) =>
+        product.id === action.payload.id
+          ? { ...product, quantity: action.payload.quantity }
+          : product,
+      )
+    },
+  },
+})
+
+export const { increaseItem, decreaseItem, changeItemQuantity } =
+  cartSlice.actions
+
+export default cartSlice.reducer
